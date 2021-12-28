b02ce3a7e35b96416877282309f8ce3842a32985
Magnetto2 <imalysev869@gmail.com>
2021-12-28 05:15:37 +0200

    Initial commit

---
+++ src/main/java/com/example/to_do__demo/ToDoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.to_do__demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/to_do__demo/ToDoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.to_do__demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
