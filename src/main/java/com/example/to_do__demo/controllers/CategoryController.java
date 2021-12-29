f7a5f7a8635c8badf27ebbfef7fff615637041e6
Magnetto2 <imalysev869@gmail.com>
2021-12-29 12:41:02 +0200

    Adding Category and Priority controllers

--- src/main/java/com/example/to_do__demo/controllers/CategoryController.java
+++ src/main/java/com/example/to_do__demo/controllers/CategoryController.java
@@ -2,12 +2,16 @@ package com.example.to_do__demo.controllers;
 
 
 import com.example.to_do__demo.Entity.CategoryEntity;
+import com.example.to_do__demo.Entity.PriorityEntity;
 import com.example.to_do__demo.repo.CategoryRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/category")
@@ -24,5 +28,59 @@ public class CategoryController {
          List<CategoryEntity> list = categoryRepository.findAll();
 
          return list;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<CategoryEntity> add(@RequestBody CategoryEntity category){
+
+        if(category.getId() != null && category.getId() == 0){
+            return new ResponseEntity("redundant:id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+        if(category.getTitle() == null && category.getTitle().trim().length() == 0){
+            return new ResponseEntity("missed param : title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+        CategoryEntity categoryEntity = categoryRepository.save(category);
+        return ResponseEntity.ok(categoryEntity);
+    }
+
+
+    @PutMapping("/update")
+    public ResponseEntity update(@RequestBody CategoryEntity category){
+
+        if(category.getId() == null || category.getId() == 0){
+            return new ResponseEntity("missed param ID", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if(category.getTitle() == null && category.getTitle().trim().length() == 0){
+            return new ResponseEntity("missed param : title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+
+
+        CategoryEntity categoryEntity = categoryRepository.save(category);
+        return ResponseEntity.ok(categoryEntity);
+
+
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable Long id){
+
+        PriorityEntity priority = null;
+
+        try{
+            categoryRepository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity(HttpStatus.OK);
+
     }
 }
--- src/main/java/com/example/to_do__demo/controllers/PriorityController.java
+++ src/main/java/com/example/to_do__demo/controllers/PriorityController.java
@@ -1,12 +1,15 @@
 package com.example.to_do__demo.controllers;
 
+import com.example.to_do__demo.Entity.CategoryEntity;
 import com.example.to_do__demo.Entity.PriorityEntity;
 import com.example.to_do__demo.repo.PriorityRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/priority")
@@ -28,6 +31,82 @@ public class PriorityController {
         return list;
     }
 
+    @PostMapping("/add")
+    public ResponseEntity<PriorityEntity> add(@RequestBody PriorityEntity priorityEntity){
 
+        if (priorityEntity.getId() != null && priorityEntity.getId() !=0){
+            return new ResponseEntity("redundant:id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+        if(priorityEntity.getTitle()==null || priorityEntity.getTitle().trim().length() == 0){
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+
+        }
+
+
+        if(priorityEntity.getColor() != null && priorityEntity.getColor().trim().length()!=0){
+            return new ResponseEntity("Missed param : color", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+        return ResponseEntity.ok(priorityRepository.save(priorityEntity));
+    }
+
+
+    @PutMapping("/update")
+    public ResponseEntity update(@RequestBody PriorityEntity priority){
+
+        if(priority.getId() == null || priority.getId() == 0){
+            return new ResponseEntity("missed param ID", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if(priority.getTitle() == null && priority.getTitle().trim().length() == 0){
+            return new ResponseEntity("missed param : title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if(priority.getColor() != null && priority.getColor().trim().length()!=0){
+            return new ResponseEntity("Missed param : color", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+
+        PriorityEntity priorityEntity = priorityRepository.save(priority);
+        return ResponseEntity.ok(priorityEntity);
+
+
+    }
+
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<PriorityEntity> findById(@PathVariable Long id){
+        PriorityEntity priority = null;
+
+        try{
+            priority = priorityRepository.findById(id).get();
+        }catch (NoSuchElementException e){
+            e.printStackTrace();
+            return new ResponseEntity("id = " + id + "not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+        return ResponseEntity.ok(priority);
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable Long id){
+
+        PriorityEntity priority = null;
+
+        try{
+            priorityRepository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity(HttpStatus.OK);
+
+    }
 
 }
